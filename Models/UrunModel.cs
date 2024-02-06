b29ec10993b8add85d09365669fabeda72ed505a
PenguBey <omer39ctnr@gmail.com>
2024-02-06 23:55:50 +0300

    Efe yardımına ihtiyacım var

---
+++ Models/UrunModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeknoFest.Models
+{
+    public class UrunModel
+    {
+        public int urunAdi { get; set; }
+    }
+}
